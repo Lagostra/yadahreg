157552d0dedc37c958102ccd85094c87546cc28c
Eivind Lie Andreassen <Eivind Lie Andreassen>
2016-09-18 09:30:22 +0200

    Added models and migrations for events and members

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eivind
+ * Date: 18/09/2016
+ * Time: 09:29
+ */
+
+namespace App\Http\Controllers;
+
+
+class RegistrationController extends Controller {
+
+}
---
+++ database/migrations/2016_09_18_065409_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('members', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('preferred_voice')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('members');
+    }
+}
