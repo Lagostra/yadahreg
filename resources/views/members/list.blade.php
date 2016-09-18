4a5a19b1b0df62cc0f016d7224489a8a05e8bd2c
Eivind Lie Andreassen <ela4991@gmail.com>
2016-09-18 10:57:35 +0200

    Needed new commit...

--- resources/views/members/list.blade.php
+++ resources/views/members/list.blade.php
@@ -37,7 +37,7 @@
                                             <button type="submit" class="btn btn-primary btn-xs">
                                                 Slett medlem
                                             </button>
-                                        </form>
+                                        </form> 
                                     </th>
 
                                 </tr>
