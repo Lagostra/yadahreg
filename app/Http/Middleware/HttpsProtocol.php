05d845831116f250609ef43dc466b075cd877809
Eivind Lie Andreassen <ela4991@gmail.com>
2016-09-18 19:31:34 +0200

    Added middleware for HTTPS forcing

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,14 @@
+namespace MyApp\Http\Middleware;
+
+use Closure;
+
+class HttpsProtocol {
+
+    public function handle($request, Closure $next) {
+        if (!$request->secure() && env('APP_ENV') === 'prod') {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
