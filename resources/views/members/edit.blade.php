9e27adf3238241cb6e04560019982522b732a63c
Eivind Lie Andreassen <ela4991@gmail.com>
2016-09-19 12:31:30 +0200

    Changed button on member edit page from "Legg til" to "Lagre"

--- resources/views/members/edit.blade.php
+++ resources/views/members/edit.blade.php
@@ -138,7 +138,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Legg til
+                                    Lagre
                                 </button>
                             </div>
 
