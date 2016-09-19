ef7568eaba4fde0fe14184049256ccbdc6d571ac
Eivind Lie Andreassen <ela4991@gmail.com>
2016-09-19 18:29:50 +0200

    Added migration to add fields to members table.

---
+++ database/migrations/2016_09_19_162343_member_add_fields.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MemberAddFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('members', function($table) {
+            $table->date('birthday')->nullable();
+            $table->string('address')->nullable();
+            $table->string('allergies')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('members', function($table) {
+            $table->dropColumn('birthday');
+            $table->dropColumn('address');
+            $table->dropColumn('allergies');
+        });
+    }
+}
