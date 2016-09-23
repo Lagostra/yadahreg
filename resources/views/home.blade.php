b90e5bdbcfdf401a4d683f0ba6f15938f6102bd6
Eivind Lie Andreassen <ela4991@gmail.com>
2016-09-23 14:24:00 +0200

    Added warning message for browsers with Javascript deactivated.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,17 @@
     <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-default">
             <div class="panel-body">
+                <noscript>
+                    <div class="alert alert-danger">
+                        <strong>Advarsel!</strong> Javascript er ikke aktivert i din nettleser. Denne siden benytter
+                        Javascript aktivt for å tilby funksjonalitet, og vil ikke fungere som forventet uten.
+                    </div>
+                </noscript>
+
                 <p>
                     Du er nå logget inn!
                 </p>
+
                 <p>
                     Bruk menyen over for å navigere.
                     Dersom du ser få/ingen lenker i menyen, kan det tyde på at brukeren din ikke er aktivert.
