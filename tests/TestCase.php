12edb141ac6f0fa606d6a4b32d4aff75828d2313
Eivind Lie Andreassen <ela4991@gmail.com>
2017-03-27 12:58:03 +0200

    Set up testing to use in memory sqlite database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,12 +10,28 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
     protected $baseUrl = 'http://localhost';
 
     /**
+     * Run before each test
+     *
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Migrate all changes to the in-memory database
+        Artisan::call('migrate');
+        // Set mail to pretend (speeds up running when sending mails)
+        Mail::pretend(true);
+    }
+
+    /**
      * Creates the application.
      *
      * @return \Illuminate\Foundation\Application
      */
     public function createApplication()
     {
+        putenv('DB_CONNECTION=sqlite_in_memory');
+
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
