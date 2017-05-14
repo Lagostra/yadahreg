ac822b6fd8df4aac51500d4815fdd77ea8e78556
Eivind Lie Andreassen <ela4991@gmail.com>
2017-05-14 20:51:47 +0200

    Toplist now uses havingRaw to enable use of COUNT

--- app/Http/Controllers/OverviewController.php
+++ app/Http/Controllers/OverviewController.php
@@ -98,7 +98,7 @@ class OverviewController extends Controller {
         }
 
         $query  ->groupBy('members.id', 'first_name', 'last_name')
-                ->having('COUNT(*)', '>', 0)
+                ->havingRaw('COUNT(*) > 0')
                 ->orderBy('num_events', 'desc');
 
         $members = $query->get();
