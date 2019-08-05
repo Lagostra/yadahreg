7fcf8a1d616ee1c07e6d247fd6cd6eed37e1f3cc
Eivind Lie Andreassen <ela4991@gmail.com>
2019-08-05 21:02:37 +0200

    Clean working directory

--- app/EventType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class EventType extends Model {
-    protected $table = "event_types";
-}
--- docs/index.md
+++
@@ -1,6 +0,0 @@
-# Index
-* [Introduction](introduction.md)
-* [Setup](setup.md)
-* [MVC](mvc.md)
-* [Migrations](migration.md)
-* [Authentication](auth.md)
