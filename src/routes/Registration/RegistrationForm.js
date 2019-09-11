0731cc1bd81704197964b3dac2b93904714c2325
Eivind Lie Andreassen <ela4991@gmail.com>
2019-09-11 20:23:36 +0200

    Implement search-to-register

--- src/routes/Registration/RegistrationForm.js
+++ src/routes/Registration/RegistrationForm.js
@@ -23,6 +23,8 @@ class RegistrationForm extends React.Component {
                 tooltip: 'Ikke til stede',
             },
         ];
+
+        this.state = { filter: '' };
     }
 
     getStatus = member => {
@@ -41,8 +43,39 @@ class RegistrationForm extends React.Component {
         return 'not-present';
     };
 
+    isMatch = (filter, name) => {
+        const regex = new RegExp('(.*)' + filter + '(.*)', 'i');
+        return regex.test(name);
+    };
+
+    handleFilterChange = e => {
+        this.setState({ filter: e.target.value });
+    };
+
+    handleFilterKeyDown = e => {
+        const { members, onRegistrationChange } = this.props;
+        const { filter } = this.state;
+        if (e.key === 'Enter') {
+            const matchingMembers = members.filter(member => {
+                const res = this.isMatch(
+                    filter,
+                    member.first_name + ' ' + member.last_name,
+                );
+                return res;
+            });
+            if (matchingMembers.length === 1) {
+                onRegistrationChange(matchingMembers[0], 'present');
+
+                setTimeout(() => {
+                    this.setState({ filter: '' });
+                }, 600);
+            }
+        }
+    };
+
     render() {
         const { members, event, onChangeEvent } = this.props;
+        const { filter } = this.state;
 
         return (
             <div className="registration-form">
@@ -57,27 +90,59 @@ class RegistrationForm extends React.Component {
                 >
                     Endre arrangement
                 </button>
+
+                <div className="registration-form__num-attendants">
+                    Antall oppmøtte:{' '}
+                    {event.attendants
+                        ? Object.keys(event.attendants).length
+                        : 0}
+                </div>
+
+                <input
+                    value={filter}
+                    onChange={this.handleFilterChange}
+                    name="filter"
+                    type="text"
+                    placeholder="Søk..."
+                    onKeyDown={this.handleFilterKeyDown}
+                />
+
                 <table className="table-full-width table-hor-lines-between">
                     <tbody>
                         {members.map(member => (
-                            <tr key={member.id}>
-                                <td className="registration-form__member-name">
-                                    {member.first_name}{' '}
-                                    {member.last_name}
-                                </td>
-                                <td className="registration-form__buttons">
-                                    <ButtonSelect
-                                        options={this.presenceOptions}
-                                        onChange={e =>
-                                            this.props.onRegistrationChange(
-                                                member,
-                                                e.value,
-                                            )
-                                        }
-                                        value={this.getStatus(member)}
-                                    />
-                                </td>
-                            </tr>
+                            <React.Fragment key={member.id}>
+                                {(!filter ||
+                                    this.isMatch(
+                                        filter,
+                                        member.first_name +
+                                            ' ' +
+                                            member.last_name,
+                                    )) && (
+                                    <tr>
+                                        <td className="registration-form__member-name">
+                                            {member.first_name}{' '}
+                                            {member.last_name}
+                                        </td>
+                                        <td className="registration-form__buttons">
+                                            <ButtonSelect
+                                                options={
+                                                    this
+                                                        .presenceOptions
+                                                }
+                                                onChange={e =>
+                                                    this.props.onRegistrationChange(
+                                                        member,
+                                                        e.value,
+                                                    )
+                                                }
+                                                value={this.getStatus(
+                                                    member,
+                                                )}
+                                            />
+                                        </td>
+                                    </tr>
+                                )}
+                            </React.Fragment>
                         ))}
                     </tbody>
                 </table>
