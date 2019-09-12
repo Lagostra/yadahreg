03a290e0aee93537e4e2abc03216d8cabf6f680c
Eivind Lie Andreassen <ela4991@gmail.com>
2019-09-12 08:52:52 +0200

    Update permission types

--- src/constants/permissions.js
+++ src/constants/permissions.js
@@ -1,4 +1,8 @@
+export const EVENT_TYPES_WRITE = 'event-types-write';
+export const EVENTS_READ = 'events-read';
+export const EVENTS_WRITE = 'events-write';
+export const MEMBER_READ = 'member-read';
+export const MEMBERS_WRITE = 'members-write';
+export const ROLES_WRITE = 'roles-write';
 export const USERS_READ = 'users-read';
 export const USERS_WRITE = 'users-write';
-export const ROLES_WRITE = 'roles-write';
-export const MEMBERS_WRITE = 'members-write';
