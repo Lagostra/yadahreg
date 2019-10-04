a158bbf81df881de0aabad349ad9daf560e4a813
Eivind Lie Andreassen <ela4991@gmail.com>
2019-10-04 13:53:05 +0200

    Fix permission typo in permissions.js

--- src/constants/permissions.js
+++ src/constants/permissions.js
@@ -1,7 +1,7 @@
 export const EVENT_TYPES_WRITE = 'event-types-write';
 export const EVENTS_READ = 'events-read';
 export const EVENTS_WRITE = 'events-write';
-export const MEMBER_READ = 'member-read';
+export const MEMBERS_READ = 'members-read';
 export const MEMBERS_WRITE = 'members-write';
 export const ROLES_WRITE = 'roles-write';
 export const USERS_READ = 'users-read';
