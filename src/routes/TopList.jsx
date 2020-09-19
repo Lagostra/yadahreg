0045208cdef2bf8df29b605017d6b3244f69d792
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2020-09-19 11:35:05 +0200

    Rewrite TopListPage to functional component

--- src/routes/TopList.jsx
+++ src/routes/TopList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { compose } from 'recompose';
 
 import { withFirebase } from '../components/Firebase';
@@ -6,165 +6,91 @@ import Spinner from '../components/Spinner';
 import moment from 'moment';
 import { withAuthorization } from '../components/Session';
 import * as PERMISSIONS from '../constants/permissions';
+import { useMembers, useEvents, useEventTypes, useSemesters } from 'hooks';
 
-class TopListPage extends React.Component {
-  constructor(props) {
-    super(props);
+const TopListPage = () => {
+  const [selectedSemester, setSelectedSemester] = useState(null);
+  const [eventTypeDrawerOpen, setEventTypeDrawerOpen] = useState(
+      false,
+  );
+  const [selectedEventTypes, setSelectedEventTypes] = useState([
+      'Øvelse',
+  ]);
+  const [filter, setFilter] = useState('');
 
-    this.state = {
-      members: [],
-      events: [],
-      eventTypes: [],
-      semesters: [],
-      selectedSemester: '',
-      eventTypeDrawerOpen: false,
-      selectedEventTypes: ['Øvelse'],
-      filter: '',
-    };
-  }
+  const [members] = useMembers();
+  const [semesters] = useSemesters();
+  const [events] = useEvents();
+  const [eventTypes] = useEventTypes();
 
-  componentDidMount() {
-    this.props.firebase.members().on('value', (snapshot) => {
-      const membersObject = snapshot.val();
-      const members = Object.keys(membersObject).map((key) => ({
-        ...membersObject[key],
-        id: key,
-      }));
-      this.setState({ members });
-    });
+  useEffect(() => {
+    setSelectedSemester(semesters[0]);
+  }, [semesters]);
 
-    this.props.firebase.events().on('value', (snapshot) => {
-      const eventsObject = snapshot.val();
-      const events = Object.keys(eventsObject).map((key) => ({
-        ...eventsObject[key],
-        id: key,
-      }));
-      this.setState({ events });
-    });
-
-    this.props.firebase.semesters().on('value', (snapshot) => {
-      const semestersObject = snapshot.val();
-      const semesters = Object.keys(semestersObject)
-        .map((key) => ({
-          ...semestersObject[key],
-          id: key,
-        }))
-        .sort((a, b) => moment(b.end_date) - moment(a.end_date));
-
-      this.setState({
-        selectedSemester: semesters[0],
-        semesters,
-      });
-    });
-
-    this.props.firebase
-      .eventTypes()
-      .once('value')
-      .then((snapshot) => {
-        const eventTypes = Object.keys(snapshot.val());
-        this.setState({ eventTypes });
-      });
-  }
-
-  componentWillUnmount() {
-    this.props.firebase.events().off();
-    this.props.firebase.semesters().off();
-    this.props.firebase.members().off();
-  }
-
-  handleSelectSemester = (selectedSemesterId) => {
-    const s = this.state.semesters.filter((s) => s.id === selectedSemesterId)[0];
-    this.setState({ selectedSemester: s });
-  };
-
-  handleEventTypeChange = (event) => {
-    let { selectedEventTypes } = this.state;
+  const handleEventTypeChange = (event) => {
     const eventType = event.currentTarget.name;
+    let newEventTypes = [...selectedEventTypes];
     if (event.currentTarget.checked && !selectedEventTypes.includes(eventType)) {
-      selectedEventTypes.push(eventType);
+      newEventTypes.push(eventType);
     } else if (!event.currentTarget.checked && selectedEventTypes.includes(eventType)) {
-      selectedEventTypes = selectedEventTypes.filter((t) => t !== eventType);
+      newEventTypes = selectedEventTypes.filter((t) => t !== eventType);
     }
-
-    this.setState({ selectedEventTypes });
-  };
-
-  handleFilterChange = (e) => {
-    this.setState({ filter: e.target.value });
-  };
-
-  render() {
-    const {
-      members,
-      events,
-      semesters,
-      selectedSemester,
-      eventTypeDrawerOpen,
-      eventTypes,
-      selectedEventTypes,
-      filter,
-    } = this.state;
-
-    const startDate =
-      semesters && semesters.length ? (selectedSemester ? selectedSemester.start_date : '1970-01-01') : null;
-    const endDate =
-      semesters && semesters.length ? (selectedSemester ? selectedSemester.end_date : '4000-01-01') : null;
-    return (
-      <div className="content">
-        <h1>Toppliste</h1>
-        <select
-          onChange={(event) => {
-            return this.handleSelectSemester(event.target.value);
-          }}
-          value={selectedSemester ? selectedSemester.id : ''}
-        >
-          <option value={''}>Totalt</option>
-          {semesters.map((semester) => (
-            <option value={semester.id} key={semester.id}>
-              {semester.title}
-            </option>
-          ))}
-        </select>
-        <div
-          onClick={() => {
-            this.setState({
-              eventTypeDrawerOpen: !eventTypeDrawerOpen,
-            });
-          }}
-          style={{ cursor: 'pointer', padding: '15px' }}
-        >
-          Typer arrangementer{' '}
-          {eventTypeDrawerOpen ? <i className="fas fa-caret-down" /> : <i className="fas fa-caret-right" />}
-        </div>
-        {eventTypeDrawerOpen && (
-          <div style={{ padding: '0 15px' }}>
-            {eventTypes.map((eventType) => (
-              <React.Fragment key={eventType}>
-                <input
-                  type="checkbox"
-                  checked={selectedEventTypes.includes(eventType)}
-                  name={eventType}
-                  onChange={this.handleEventTypeChange}
-                  className="inline"
-                />
-                {eventType}
-              </React.Fragment>
-            ))}
-          </div>
-        )}
-        <input value={filter} onChange={this.handleFilterChange} name="filter" type="text" placeholder="Søk..." />
-        <TopList
-          members={members}
-          events={events}
-          eventTypes={selectedEventTypes}
-          startDate={startDate}
-          endDate={endDate}
-          filter={filter}
-        />
-      </div>
-    );
+    setSelectedEventTypes(newEventTypes);
   }
-}
+
+  const startDate =
+  semesters && semesters.length ? (selectedSemester ? selectedSemester.start_date : '1970-01-01') : null;
+  const endDate =
+    semesters && semesters.length ? (selectedSemester ? selectedSemester.end_date : '4000-01-01') : null;
+  return (
+    <div className="content">
+      <h1>Toppliste</h1>
+      <select
+        onChange={(event) => setSelectedSemester(semesters.filter(semester => semester.id === event.target.value)[0])}
+        value={selectedSemester ? selectedSemester.id : ''}
+      >
+        <option value={null}>Totalt</option>
+        {semesters.map((semester) => (
+          <option value={semester.id} key={semester.id}>
+            {semester.title}
+          </option>
+        ))}
+      </select>
+      <div
+        onClick={() => setEventTypeDrawerOpen(!eventTypeDrawerOpen)}
+        style={{ cursor: 'pointer', padding: '15px' }}
+      >
+        Typer arrangementer{' '}
+        {eventTypeDrawerOpen ? <i className="fas fa-caret-down" /> : <i className="fas fa-caret-right" />}
+      </div>
+      {eventTypeDrawerOpen && (
+        <div style={{ padding: '0 15px' }}>
+          {eventTypes.map((eventType) => (
+            <React.Fragment key={eventType}>
+              <input
+                type="checkbox"
+                checked={selectedEventTypes.includes(eventType)}
+                name={eventType}
+                onChange={handleEventTypeChange}
+                className="inline"
+              />
+              {eventType}
+            </React.Fragment>
+          ))}
+        </div>
+      )}
+      <input value={filter} onChange={e => setFilter(e.target.value)} name="filter" type="text" placeholder="Søk..." />
+      <TopList
+        members={members}
+        events={events}
+        eventTypes={selectedEventTypes}
+        startDate={startDate}
+        endDate={endDate}
+        filter={filter}
+      />
+    </div>
+  );
+};
 
 const TopList = ({ members, events, startDate, endDate, eventTypes = ['Øvelse'], filter }) => {
   if (!members || !events || !startDate || !endDate) {
