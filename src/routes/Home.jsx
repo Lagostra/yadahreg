084ebf97257614d2d61608d93fb3ac4650cf65f8
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2020-09-23 18:52:50 +0200

    Use hooks in home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { compose } from 'recompose';
-import { withAuthorization, withAuthUser } from '../components/Session';
+import { useAuthUser } from 'hooks';
 
-const Home = ({ authUser }) => (
-  <div className="content">
-    <h1>
-      Velkommen til <strong>YadahReg!</strong>
-    </h1>
-    {authUser.permissions && authUser.role ? (
-      <p>Bruk toppmenyen for å navigere til de forskjellige verktøyene på siden.</p>
-    ) : (
-      <p>Brukeren din er ikke aktivert. Ta kontakt med en administrator for å få fikset dette.</p>
-    )}
-  </div>
-);
+const Home = () => {
+ 
+  const authUser = useAuthUser();
 
-export default compose(
-  withAuthorization((authUser) => !!authUser),
-  withAuthUser,
-)(Home);
+  return (
+    <div className="content">
+      <h1>
+        Velkommen til <strong>YadahReg!</strong>
+      </h1>
+      {authUser.permissions && authUser.role ? (
+        <p>Bruk toppmenyen for å navigere til de forskjellige verktøyene på siden.</p>
+      ) : (
+        <p>Brukeren din er ikke aktivert. Ta kontakt med en administrator for å få fikset dette.</p>
+      )}
+    </div>
+  )
+};
+
+export default Home;
