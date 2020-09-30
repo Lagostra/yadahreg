d5587c96effac70d4febc6cdfcc6d64ee9b06f84
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2020-09-30 20:11:02 +0200

    Rewrite InactiveMembers as function component

--- src/routes/InactiveMembers.jsx
+++ src/routes/InactiveMembers.jsx
@@ -1,93 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
-import { compose } from 'recompose';
 
-import { withFirebase } from '../components/Firebase';
 import Spinner from '../components/Spinner';
 import Modal from '../components/Modal';
 import { MemberForm } from './Members';
 import * as PERMISSIONS from '../constants/permissions';
 import { withAuthorization } from '../components/Session';
-import { withAuthUser } from '../components/Session';
+import { useEvents, useFirebase, useMembers, useSemesters } from 'hooks';
 
-class InactiveMembers extends React.Component {
-  constructor(props) {
-    super(props);
+const InactiveMembers = () => {  
+  const [semesters] = useSemesters();
+  const [members] = useMembers();
+  const [events] = useEvents();
 
-    this.state = {
-      inactiveMembers: [],
-      loaded: false,
-      modalActive: false,
-      editMember: null,
-      semester: null,
-    };
+  const firebase = useFirebase();
+
+  const [modalActive, setModalActive] = useState(false);
+  const [editMember, setEditMember] = useState(null);
+  const [semester, setSemester] = useState(null);
+
+  const getFilteredEvents = () => {
+    if (!events) return null;
+    const lastThree = events.slice(0, 3);
+    const thisYear = moment().year();
+    const semesterStart =
+        moment() <= moment([thisYear, 6, 31])
+            ? moment([thisYear, 0, 1])
+            : moment([thisYear, 7, 1]);
+    let filteredEvents = events.filter(
+        event => moment(event.date) > semesterStart,
+    );
+    filteredEvents = filteredEvents.length >= 3 ? filteredEvents : lastThree;
+    return filteredEvents;
   }
 
-  componentDidMount() {
-    const semesterPromise =
-      !!this.props.authUser && !!this.props.authUser.permissions[PERMISSIONS.SEMESTERS_READ]
-        ? this.props.firebase
-            .semesters()
-            .once('value')
-            .then((snapshot) => {
-              const semestersObject = snapshot.val();
-              const semesters = Object.keys(semestersObject).map((key) => ({
-                ...semestersObject[key],
-                id: key,
-              }));
+  const filteredEvents = getFilteredEvents();
 
-              const lastSemester = semesters.reduce((a, b) => (moment(a.end_date) > moment(b.end_data) ? a : b));
-
-              this.setState({ semester: lastSemester });
-            })
-        : Promise.resolve();
-
-    const memberPromise = this.props.firebase
-      .members()
-      .once('value')
-      .then((snapshot) => {
-        const membersObject = snapshot.val();
-        const members = Object.keys(membersObject)
-          .map((key) => ({
-            ...membersObject[key],
-            id: key,
-          }))
-          .filter((m) => m.active);
-
-        return members;
-      });
-
-    const eventsPromise = this.props.firebase
-      .events()
-      .once('value')
-      .then((snapshot) => {
-        const eventsObject = snapshot.val();
-        let events = Object.keys(eventsObject)
-          .map((key) => ({
-            ...eventsObject[key],
-            id: key,
-          }))
-          .filter((event) => event.type && event.type === 'Øvelse')
-          .sort((a, b) => moment(b.date) - moment(a.date));
-
-        const lastThree = events.slice(0, 3);
-        const thisYear = moment().year();
-        const semesterStart =
-          moment() <= moment([thisYear, 6, 31]) ? moment([thisYear, 0, 1]) : moment([thisYear, 7, 1]);
-        events = events.filter((event) => moment(event.date) > semesterStart);
-        events = events.length >= 3 ? events : lastThree;
-
-        return events;
-      });
-
-    Promise.all([memberPromise, eventsPromise, semesterPromise]).then(([members, events]) => {
-      members = members.map((member) => {
-        let totalAbsent = 0;
-        let absentFromLast = true;
-        let absentInRow = 0;
-        let maxAbsentInRow = 0;
-        let lastPresent = null;
-        events.forEach((event) => {
+  const inactiveMembers = members
+    ?.map((member) => {
+      let totalAbsent = 0;
+      let absentFromLast = true;
+      let absentInRow = 0;
+      let maxAbsentInRow = 0;
+      let lastPresent = null;
+      if (events) {
+        filteredEvents.forEach((event) => {
           if (!event.attendants || !event.attendants[member.id]) {
             totalAbsent += 1;
 
@@ -108,115 +65,107 @@ class InactiveMembers extends React.Component {
               lastPresent = event.date;
             }
           }
-        });
-
-        const inactive = maxAbsentInRow >= 3 || totalAbsent >= 6;
-
-        return {
-          ...member,
-          inactive,
-          lastPresent,
-          absentInRow: maxAbsentInRow,
-          totalAbsent,
-        };
-      });
-
-      const inactiveMembers = members
-        .filter((m) => m.inactive)
-        .sort((a, b) => b.totalAbsent - a.totalAbsent)
-        .sort((a, b) => b.absentInRow - a.absentInRow)
-        .sort((a, b) => {
-          if (!b.lastPresent && !a.lastPresent) {
-            return 0;
-          }
-          if (!b.lastPresent) {
-            return 1;
-          }
-          if (!a.lastPresent) {
-            return -1;
-          }
-          return moment(a.lastPresent) - moment(b.lastPresent);
-        });
-
-      this.setState({ inactiveMembers, loaded: true });
+        })
+      }
+      
+      const inactive = maxAbsentInRow >= 3 || totalAbsent >= 6;
+      
+      return {
+        ...member,
+        inactive,
+        lastPresent,
+        absentInRow: maxAbsentInRow,
+        totalAbsent,
+      };
+    })
+    .filter((m) => m.inactive)
+    .sort((a, b) => b.totalAbsent - a.totalAbsent)
+    .sort((a, b) => b.absentInRow - a.absentInRow)
+    .sort((a, b) => {
+      if (!b.lastPresent && !a.lastPresent) {
+        return 0;
+      }
+      if (!b.lastPresent) {
+        return 1;
+      }
+      if (!a.lastPresent) {
+        return -1;
+      }
+      return moment(a.lastPresent) - moment(b.lastPresent);
     });
-  }
 
-  handleModalClose = (memberId) => {
-    this.setState({ modalActive: false });
+  const handleModalClose = (memberId) => {
     if (memberId) {
-      this.props.firebase
+      firebase
         .member(memberId)
         .once('value')
         .then((snapshot) => {
-          if (!snapshot.val().active) {
-            this.setState({
-              inactiveMembers: this.state.inactiveMembers.filter((m) => m.id !== memberId),
-            });
-          }
+          setModalActive(false);
         });
+    } else {
+      setModalActive(false);
     }
   };
 
-  handleEditMember = (member) => {
-    this.setState({ editMember: member, modalActive: true });
-  };
+  useEffect(() => {
+    if (semesters) {
+      setSemester(semesters[semesters.length - 1]);
+    }
+  }, [semesters]);
 
-  render() {
-    const { inactiveMembers, loaded, modalActive, editMember, semester } = this.state;
+  const loaded = !!events && !!members;
 
-    return (
-      <React.Fragment>
-        <Modal active={modalActive} onClose={this.handleModalClose}>
-          <MemberForm member={editMember} onSubmit={this.handleModalClose} />
-        </Modal>
-        <div className="content">
-          <h1>Inaktive medlemmer</h1>
-          {!loaded && <Spinner />}
-          {loaded && 'Medlemskapet til de følgende kormedlemmene er regnet som avsluttet jf. korets vedtekter (§3.5).'}
-          {loaded && !!inactiveMembers.length && (
-            <table className="table-full-width table-hor-lines-between">
-              <thead>
-                <tr>
-                  <th>Navn</th>
-                  <th>Siste øvelse</th>
-                  <th className="desktop-only">Fravær dette semester</th>
-                  <th className="desktop-only">Fravær på rad</th>
-                  {semester && <th className="desktop-only">Betalt semesteravgift</th>}
-                  <th></th>
+  return (
+    <>
+      <Modal active={modalActive} onClose={handleModalClose}>
+        <MemberForm member={editMember} onSubmit={handleModalClose} />
+      </Modal>
+      <div className="content">
+        <h1>Inaktive medlemmer</h1>
+        {!loaded && <Spinner />}
+        {loaded && 'Medlemskapet til de følgende kormedlemmene er regnet som avsluttet jf. korets vedtekter (§3.5).'}
+        {loaded && !!inactiveMembers.length && (
+          <table className="table-full-width table-hor-lines-between">
+            <thead>
+              <tr>
+                <th>Navn</th>
+                <th>Siste øvelse</th>
+                <th className="desktop-only">Fravær dette semester</th>
+                <th className="desktop-only">Fravær på rad</th>
+                {semester && <th className="desktop-only">Betalt semesteravgift</th>}
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {inactiveMembers.map((member) => (
+                <tr key={member.id}>
+                  <td>
+                    {member.first_name} {member.last_name}
+                  </td>
+                  <td>
+                    {member.lastPresent ? moment(member.lastPresent).format('DD.MM.YYYY') : 'Ingen dette semesteret'}
+                  </td>
+                  <td className="desktop-only">{member.totalAbsent}</td>
+                  <td className="desktop-only">{member.absentInRow}</td>
+                  {semester && (
+                    <td className="desktop-only">{semester.payees && semester.payees[member.id] ? 'Ja' : 'Nei'}</td>
+                  )}
+                  <td>
+                    <button className="btn btn-small" onClick={() => {setEditMember(member); setModalActive(true);}}>
+                      <i className="fas fa-edit" />
+                    </button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {inactiveMembers.map((member) => (
-                  <tr key={member.id}>
-                    <td>
-                      {member.first_name} {member.last_name}
-                    </td>
-                    <td>
-                      {member.lastPresent ? moment(member.lastPresent).format('DD.MM.YYYY') : 'Ingen dette semesteret'}
-                    </td>
-                    <td className="desktop-only">{member.totalAbsent}</td>
-                    <td className="desktop-only">{member.absentInRow}</td>
-                    {semester && (
-                      <td className="desktop-only">{semester.payees && semester.payees[member.id] ? 'Ja' : 'Nei'}</td>
-                    )}
-                    <td>
-                      <button className="btn btn-small" onClick={() => this.handleEditMember(member)}>
-                        <i className="fas fa-edit" />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>
-      </React.Fragment>
-    );
-  }
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
 }
 
 const authCondition = (authUser) =>
   !!authUser && !!authUser.permissions[PERMISSIONS.MEMBERS_READ] && !!authUser.permissions[PERMISSIONS.EVENTS_READ];
 
-export default compose(withFirebase, withAuthorization(authCondition), withAuthUser)(InactiveMembers);
+export default withAuthorization(authCondition)(InactiveMembers);
