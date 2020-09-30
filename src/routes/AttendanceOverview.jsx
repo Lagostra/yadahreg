e8d301684e8335144a63c48738fb88b94e062518
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2020-09-30 19:40:14 +0200

    Rewrite AttendanceOverview as function component

--- src/routes/AttendanceOverview.jsx
+++ src/routes/AttendanceOverview.jsx
@@ -1,83 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
-import { compose } from 'recompose';
 import XLSX from 'xlsx';
 
-import { withFirebase } from '../components/Firebase';
 import Spinner from '../components/Spinner';
 import { withAuthorization } from '../components/Session';
 import * as PERMISSIONS from '../constants/permissions';
+import { useEvents, useMembers } from 'hooks';
 
-class AttendanceOverview extends React.Component {
-  constructor(props) {
-    super(props);
+const AttendanceOverview = () => {
+  const [members] = useMembers();
+  const [events] = useEvents();
+  const [startDate, setStartDate] = useState(moment().subtract(30, 'd').toDate());
+  const [endDate, setEndDate] = useState(moment().add(1, 'd').toDate());
+  const [filter, setFilter] = useState('');
 
-    this.state = {
-      members: [],
-      events: [],
-      startDate: moment().subtract(30, 'd').toDate(),
-      endDate: moment().add(1, 'd').toDate(),
-      filter: '',
-    };
-  }
-
-  componentDidMount() {
-    this.props.firebase.members().on('value', (snapshot) => {
-      const membersObject = snapshot.val();
-      const members = Object.keys(membersObject).map((key) => ({
-        ...membersObject[key],
-        uid: key,
-      }));
-
-      members.sort((a, b) => (a.first_name + ' ' + a.last_name).localeCompare(b.first_name + ' ' + b.last_name));
-
-      this.setState({ members });
-    });
-
-    this.props.firebase.events().on('value', (snapshot) => {
-      const eventsObject = snapshot.val();
-      const events = Object.keys(eventsObject).map((key) => ({
-        ...eventsObject[key],
-        uid: key,
-      }));
-
-      events.sort((a, b) => moment(a.date) - moment(b.date));
-
-      this.setState({ events });
-    });
-  }
-
-  componentWillUnmount() {
-    this.props.firebase.members().off();
-    this.props.firebase.events().off();
-  }
-
-  isMatch = (filter, name) => {
+  const isMatch = (filter, name) => {
     const regex = new RegExp('(.*)' + filter + '(.*)', 'i');
     return regex.test(name);
   };
 
-  handleFilterChange = (e) => {
-    this.setState({ filter: e.target.value });
-  };
+  const filteredEvents = events?.filter(
+    (event) => moment(event.date) > moment(startDate) && moment(event.date) < moment(endDate),
+  );
 
-  handleDateChange = (e) => {
-    this.setState({
-      [e.currentTarget.name]: moment(e.currentTarget.value).toDate(),
-    });
-  };
+  const filteredMembers = members?.filter(
+    (member) => member.active && (!filter || isMatch(filter, member.first_name + ' ' + member.last_name)),
+  );
 
-  handleDownload = (e) => {
-    const { members, events, startDate, endDate, filter } = this.state;
-
-    const filteredEvents = events.filter(
-      (event) => moment(event.date) > moment(startDate) && moment(event.date) < moment(endDate),
-    );
-
-    const filteredMembers = members.filter(
-      (member) => member.active && (!filter || this.isMatch(filter, member.first_name + ' ' + member.last_name)),
-    );
-
+  const handleDownload = (e) => {
     const presence = filteredMembers.map((member) => {
       const row = {
         Etternavn: member.last_name,
@@ -111,92 +61,79 @@ class AttendanceOverview extends React.Component {
     });
   };
 
-  render() {
-    const { members, events, startDate, endDate, filter } = this.state;
-
-    const filteredEvents = events.filter(
-      (event) => moment(event.date) > moment(startDate) && moment(event.date) < moment(endDate),
-    );
-
-    const filteredMembers = members.filter(
-      (member) => member.active && (!filter || this.isMatch(filter, member.first_name + ' ' + member.last_name)),
-    );
-
-    return (
-      <div className="content">
-        {(!members.length || !events.length) && <Spinner />}
-        {!!members.length && !!events.length && (
-          <React.Fragment>
-            <button className="btn" onClick={this.handleDownload}>
-              Last ned som Excel
-            </button>
-            <div className="row">
-              <div className="col-half">
-                <label htmlFor="startDate">Startdato</label>
-                <input
-                  name="startDate"
-                  value={moment(startDate).format('YYYY-MM-DD')}
-                  onChange={this.handleDateChange}
-                  type="date"
-                />
-              </div>
-
-              <div className="col-half">
-                <label htmlFor="endDate">Sluttdato</label>
-                <input
-                  name="endDate"
-                  value={moment(endDate).format('YYYY-MM-DD')}
-                  onChange={this.handleDateChange}
-                  type="date"
-                />
-              </div>
+  return (
+    <div className="content">
+      {(!members || !events) && <Spinner />}
+      {!!members && !!events && (
+        <React.Fragment>
+          <button className="btn" onClick={handleDownload}>
+            Last ned som Excel
+          </button>
+          <div className="row">
+            <div className="col-half">
+              <label htmlFor="startDate">Startdato</label>
+              <input
+                name="startDate"
+                value={moment(startDate).format('YYYY-MM-DD')}
+                onChange={e => setStartDate(moment(e.currentTarget.value).toDate())}
+                type="date"
+              />
             </div>
-            <input value={filter} onChange={this.handleFilterChange} name="filter" type="text" placeholder="Søk..." />
-            <div className="table-scroll-container">
-              <table className="table-full-width table-hor-lines-between">
-                <thead>
-                  <tr>
-                    <th>Navn</th>
-                    {filteredEvents.map((event) => (
-                      <th key={event.uid}>{moment(event.date).format('DD.MM.YYYY')}</th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
+
+            <div className="col-half">
+              <label htmlFor="endDate">Sluttdato</label>
+              <input
+                name="endDate"
+                value={moment(endDate).format('YYYY-MM-DD')}
+                onChange={e => setEndDate(moment(e.currentTarget.value).toDate())}
+                type="date"
+              />
+            </div>
+          </div>
+          <input value={filter} onChange={e => setFilter(e.target.value)} name="filter" type="text" placeholder="Søk..." />
+          <div className="table-scroll-container">
+            <table className="table-full-width table-hor-lines-between">
+              <thead>
+                <tr>
+                  <th>Navn</th>
+                  {filteredEvents.map((event) => (
+                    <th key={event.uid}>{moment(event.date).format('DD.MM.YYYY')}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <i>Antall oppmøtte</i>
+                  </td>
+                  {filteredEvents.map((event) => (
+                    <td key={event.uid}>{event.attendants ? Object.keys(event.attendants).length : 0}</td>
+                  ))}
+                </tr>
+                {filteredMembers.map((member) => (
+                  <tr key={member.uid}>
                     <td>
-                      <i>Antall oppmøtte</i>
+                      {member.first_name} {member.last_name}
                     </td>
                     {filteredEvents.map((event) => (
-                      <td key={event.uid}>{event.attendants ? Object.keys(event.attendants).length : 0}</td>
+                      <td key={event.uid}>
+                        {event.attendants && member.id in event.attendants
+                          ? 'Y'
+                          : event.non_attendants && member.id in event.non_attendants
+                          ? 'N'
+                          : ''}
+                      </td>
                     ))}
                   </tr>
-                  {filteredMembers.map((member) => (
-                    <tr key={member.uid}>
-                      <td>
-                        {member.first_name} {member.last_name}
-                      </td>
-                      {filteredEvents.map((event) => (
-                        <td key={event.uid}>
-                          {event.attendants && !!event.attendants[member.uid]
-                            ? 'Y'
-                            : event.non_attendants && !!event.non_attendants[member.uid]
-                            ? 'N'
-                            : ''}
-                        </td>
-                      ))}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </React.Fragment>
-        )}
-      </div>
-    );
-  }
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </React.Fragment>
+      )}
+    </div>
+  );
 }
-
 const authCondition = (authUser) => !!authUser && !!authUser.permissions[PERMISSIONS.EVENTS_WRITE];
 
-export default compose(withFirebase, withAuthorization(authCondition))(AttendanceOverview);
+export default withAuthorization(authCondition)(AttendanceOverview);
