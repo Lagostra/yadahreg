f8786b11b38aeb3649413ef1e8b636072ece6787
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2020-10-03 12:40:26 +0200

    Rewrite SemesterStatistics as a function component

--- src/routes/SemesterStatistics.jsx
+++ src/routes/SemesterStatistics.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { compose } from 'recompose';
 import moment from 'moment';
 import jStat from 'jStat';
@@ -8,8 +8,87 @@ import { withAuthorization } from '../components/Session';
 import * as PERMISSIONS from '../constants/permissions';
 import Spinner from '../components/Spinner';
 import round from '../util/round';
+import { useEvents, useSemesters } from 'hooks';
 
-class SemesterStatistics extends React.Component {
+const SemesterStatistics = () => {
+  const [semesters] = useSemesters();
+  const [events] = useEvents();
+  const [selectedSemester, setSelectedSemester] = useState(null);
+
+  const filteredEvents = events?.filter((event) => {
+    const d = moment(event.date);
+    return (
+      (!selectedSemester || (d >= moment(selectedSemester.start_date) && d <= moment(selectedSemester.end_date))) &&
+      event.type === 'Øvelse'
+    );
+  });
+
+  const attendance = filteredEvents?.map((e) => (e.attendants ? Object.keys(e.attendants).length : 0));
+
+  const meanAttendance = attendance ? round(jStat.mean(attendance), 2) : null;
+  const medianAttendance = attendance ? jStat.median(attendance) : null;
+  let modeAttendance = attendance ? jStat.mode(attendance) : null;
+  if (Array.isArray(modeAttendance)) {
+    modeAttendance = modeAttendance.join(', ');
+  }
+
+  const maxAttendance = attendance ? jStat.max(attendance) : null;
+  const minAttendance = attendance ? jStat.min(attendance) : null;
+
+  return (
+    <div className="content">
+      <h1>Semesterstatistikk</h1>
+
+      {(!semesters || !events) && <Spinner />}
+      {!!semesters && !!events && (
+        <>
+          <select
+            onChange={(e) => setSelectedSemester(semesters?.find(s => s.uid === e.target.value))}
+            value={selectedSemester ? selectedSemester.uid : ''}
+          >
+            <option value={''}>Totalt</option>
+            {semesters.map((semester) => (
+              <option value={semester.uid} key={semester.uid}>
+                {semester.title}
+              </option>
+            ))}
+          </select>
+
+          <table className="table-full-width table-hor-lines-between">
+            <tbody>
+              <tr>
+                <td>Antall øvelser</td>
+                <td>{filteredEvents.length}</td>
+              </tr>
+              <tr>
+                <td>Gjennomsnittlig oppmøte</td>
+                <td>{meanAttendance}</td>
+              </tr>
+              <tr>
+                <td>Maksimalt oppmøte</td>
+                <td>{maxAttendance}</td>
+              </tr>
+              <tr>
+                <td>Minimalt oppmøte</td>
+                <td>{minAttendance}</td>
+              </tr>
+              <tr>
+                <td>Median</td>
+                <td>{medianAttendance}</td>
+              </tr>
+              <tr>
+                <td>Typetall</td>
+                <td>{modeAttendance}</td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  );
+}
+
+class OldSemesterStatistics extends React.Component {
   constructor(props) {
     super(props);
 
