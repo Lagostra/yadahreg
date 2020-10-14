80286a360efd0516b55e6d63aa024163605dd838
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2020-10-14 19:55:44 +0200

    Rewrite PaymentForm as funciton component

--- src/routes/Payment/PaymentForm.jsx
+++ src/routes/Payment/PaymentForm.jsx
@@ -1,46 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonSelect from '../../components/ButtonSelect';
 
-class PaymentForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.paymentOptions = [
-      {
-        value: 'paid',
-        text: (
-          <i
-            className="fas fa-money-bill"
-            style={{
-              fontSize: '1.5em',
-              lineHeight: '1em',
-            }}
-          />
-        ),
-        toolTip: 'Har betalt',
-      },
-      {
-        value: 'not-paid',
-        text: (
-          <span
-            className="fa-stack"
-            style={{
-              fontSize: '0.75em',
-            }}
-          >
-            <i className="fas fa-money-bill fa-stack-1x" />
-            <i className="fas fa-ban fa-stack-2x" />
-          </span>
-        ),
-        toolTip: 'Har ikke betalt',
-      },
-    ];
+const PAYMENT_OPTIONS = [
+  {
+    value: 'paid',
+    text: (
+      <i
+        className="fas fa-money-bill"
+        style={{
+          fontSize: '1.5em',
+          lineHeight: '1em',
+        }}
+      />
+    ),
+    toolTip: 'Har betalt',
+  },
+  {
+    value: 'not-paid',
+    text: (
+      <span
+        className="fa-stack"
+        style={{
+          fontSize: '0.75em',
+        }}
+      >
+        <i className="fas fa-money-bill fa-stack-1x" />
+        <i className="fas fa-ban fa-stack-2x" />
+      </span>
+    ),
+    toolTip: 'Har ikke betalt',
+  },
+];
 
-    this.state = { filter: '' };
-  }
+const PaymentForm = ({ semester, members, onPaymentChange, onChangeSemester }) => {
+  const [filter, setFilter] = useState('');
 
-  getStatus = (member) => {
-    const { semester } = this.props;
-
+  const getStatus = (member) => {
     if (semester) {
       if (semester.payees && !!semester.payees[member.id]) {
         return 'paid';
@@ -49,18 +44,12 @@ class PaymentForm extends React.Component {
     return 'not-paid';
   };
 
-  isMatch = (filter, name) => {
+  const isMatch = (filter, name) => {
     const regex = new RegExp('(.*)' + filter + '(.*)', 'i');
     return regex.test(name);
   };
 
-  handleFilterChange = (e) => {
-    this.setState({ filter: e.target.value });
-  };
-
-  handleFilterKeyDown = (e) => {
-    const { members, onPaymentChange } = this.props;
-    const { filter } = this.state;
+  const handleFilterKeyDown = (e) => {
     if (e.key === 'Enter') {
       const matchingMembers = members.filter((member) => {
         const res = this.isMatch(filter, member.first_name + ' ' + member.last_name);
@@ -69,61 +58,54 @@ class PaymentForm extends React.Component {
       if (matchingMembers.length === 1) {
         onPaymentChange(matchingMembers[0], 'paid');
 
-        setTimeout(() => {
-          this.setState({ filter: '' });
-        }, 600);
+        setTimeout(() => setFilter(''), 600);
       }
     }
   };
 
-  render() {
-    const { members, semester, onChangeSemester } = this.props;
-    const { filter } = this.state;
-
-    return (
-      <div className="payment-form">
-        <div className="payment-form__header">
-          <h1>{semester.title}</h1>
-        </div>
-
-        <button className="btn btn-link payment-form__change-semester" onClick={onChangeSemester}>
-          Endre semester
-        </button>
-
-        <input
-          value={filter}
-          onChange={this.handleFilterChange}
-          onKeyDown={this.handleFilterKeyDown}
-          name="filter"
-          type="text"
-          placeholder="Søk..."
-        />
-
-        <table className="payment-form__member-table table-full-width table-hor-lines-between table-last-td-right">
-          <tbody>
-            {members.map((member) => (
-              <React.Fragment key={member.id}>
-                {(!filter || this.isMatch(filter, member.first_name + ' ' + member.last_name)) && (
-                  <tr>
-                    <td className="payment-form__member-name">
-                      {member.first_name} {member.last_name}
-                    </td>
-                    <td className="payment-form__buttons">
-                      <ButtonSelect
-                        options={this.paymentOptions}
-                        onChange={(e) => this.props.onPaymentChange(member, e.value)}
-                        value={this.getStatus(member)}
-                      />
-                    </td>
-                  </tr>
-                )}
-              </React.Fragment>
-            ))}
-          </tbody>
-        </table>
+  return (
+    <div className="payment-form">
+      <div className="payment-form__header">
+        <h1>{semester.title}</h1>
       </div>
-    );
-  }
-}
+
+      <button className="btn btn-link payment-form__change-semester" onClick={onChangeSemester}>
+        Endre semester
+      </button>
+
+      <input
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        onKeyDown={handleFilterKeyDown}
+        name="filter"
+        type="text"
+        placeholder="Søk..."
+      />
+
+      <table className="payment-form__member-table table-full-width table-hor-lines-between table-last-td-right">
+        <tbody>
+          {members.map((member) => (
+            <React.Fragment key={member.id}>
+              {(!filter || isMatch(filter, member.first_name + ' ' + member.last_name)) && (
+                <tr>
+                  <td className="payment-form__member-name">
+                    {member.first_name} {member.last_name}
+                  </td>
+                  <td className="payment-form__buttons">
+                    <ButtonSelect
+                      options={PAYMENT_OPTIONS}
+                      onChange={(e) => onPaymentChange(member, e.value)}
+                      value={getStatus(member)}
+                    />
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default PaymentForm;
